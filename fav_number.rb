96bcf9daa9eceafea4173477ac68546e7cf7e6fb
Uthaeus <romanlavery@gmail.com>
2018-03-23 14:53:25 -0600

    Favorite number

---
+++ fav_number.rb
@@ -0,0 +1,14 @@
+puts "Welcome!"
+puts "_______________________"
+puts ""
+puts "Please enter your favorite number."
+num = gets.chomp.to_i
+sleep 1
+
+puts ""
+puts ""
+puts "Your favorite number is #{num}."
+sleep 1
+puts ""
+puts "But don't you think #{num + 1} is a bigger and better number?"
+puts ""
