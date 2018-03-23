b647d0ec8a47976ab40d6e618fc0e1324903479d
Uthaeus <romanlavery@gmail.com>
2018-03-23 14:47:49 -0600

    Name exercise

---
+++ name.rb
@@ -0,0 +1,16 @@
+puts "Welcome!"
+puts "---------------------"
+puts ""
+puts "Please enter your first name."
+first = gets.chomp
+puts "Please enter your middle name."
+middle = gets.chomp
+puts "Please enter your last name"
+last = gets.chomp
+
+sleep 1
+puts ""
+puts ""
+puts "Hello there #{first} #{middle} #{last}!"
+puts ""
+puts "Nice to meet you!"
